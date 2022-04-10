ed74fe231708aee4c568e910a6cb18f9cd254804
hshirk <hshirk@uw.edu>
2022-04-10 02:29:11 -0700

    finished whole layout

--- personal-website/index.js
+++ personal-website/index.js
@@ -11,9 +11,23 @@
   const tab = qsa('[data-target]');
   const tabContents = qsa('[data-content]');
 
-  const modalViews = qsa('.services_modal'),
-        modalBtns = qsa('.services_button'),
-        modalCloses = qsa('.services_modal-close');
+  const modalViews = qsa('.services_modal');
+  const modalBtns = qsa('.services_button');
+  const modalCloses = qsa('.services_modal-close');
+
+  const sections = document.querySelectorAll('section[id]');
+
+  const themeButton = document.getElementById('theme-button')
+  const darkTheme = 'dark-theme'
+  const iconTheme = 'uil-sun'
+
+  // Previously selected topic (if user selected)
+  const selectedTheme = localStorage.getItem('selected-theme')
+  const selectedIcon = localStorage.getItem('selected-icon')
+
+  // We obtain the current theme that the interface has by validating the dark-theme class
+  const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+  const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
   window.addEventListener("load", init);
 
@@ -30,7 +44,9 @@
     }
     navLink.forEach(n => n.addEventListener('click', linkAction));
 
-    skillsHeader.forEach(el => el.addEventListener('click', toggleSkills));
+    skillsHeader.forEach((el) => {
+      el.addEventListener('click', toggleSkills)
+    });
 
     tabContents.forEach(tab => {
       tab.addEventListener('click', addQualification);
@@ -48,7 +64,6 @@
         });
       });
     });
-
     /*================== PORTFOLIO SWIPER ====================*/
     let swiper = new Swiper('.portfolio_container', {
       cssMode: true,
@@ -62,6 +77,28 @@
         el: '.swiper-pagination',
         clickable: true,
       },
+    });
+
+    window.addEventListener('scroll', scrollActive);
+    window.addEventListener('scroll', scrollHeader);
+    window.addEventListener('scroll', scrollUp);
+
+    /*==================== DARK LIGHT THEME ====================*/
+    // We validate if the user previously chose a topic
+    if (selectedTheme) {
+      // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+      document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+      themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    }
+
+    // Activate / deactivate the theme manually with the button
+    themeButton.addEventListener('click', () => {
+        // Add or remove the dark / icon theme
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+        localStorage.setItem('selected-theme', getCurrentTheme())
+        localStorage.setItem('selected-icon', getCurrentIcon())
     });
   }
 
@@ -100,7 +137,36 @@
     modalViews[modalClick].classList.add('active-modal');
   }
 
+  /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+  function scrollActive(){
+      let scrollY = window.pageYOffset;
 
+      sections.forEach(current =>{
+          let sectionHeight = current.offsetHeight;
+          let sectionTop = current.offsetTop - 50;
+          sectionId = current.getAttribute('id')
+
+          if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+              qs('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+          } else {
+              qs('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+          }
+      })
+  }
+
+  /*==================== CHANGE BACKGROUND HEADER ====================*/
+  function scrollHeader(){
+    let nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  }
+
+  /*==================== SHOW SCROLL UP ====================*/
+  function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  }
 
   /**
    * Returns the element that has the ID attribute with the specified value.
